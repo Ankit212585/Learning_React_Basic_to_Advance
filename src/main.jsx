267094ede6949efd73dbb77e0e0958c1db8ac168
Ankit Panwar <ankitpanwar212585@gmail.com>
2024-07-09 12:24:03 +0530

    update all

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import Index from "./practic folder/index";
-// import Todo from "./todo";
+import Todo from "./todo";
 // import Food from "./New folder/food";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import Calculator from "./Calculator";
-import SocailMedia from "./SocialMedia/SocailMedia";
+// import SocailMedia from "./SocialMedia/SocailMedia";
 // import ReactStart from "./practic folder/ReactStart";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <Index /> */}
     {/* <ReactStart /> */}
-    {/* <Todo /> */}
+    <Todo />
     {/* <Food /> */}
     {/* <Calculator /> */}
-    <SocailMedia />
+    {/* <SocailMedia /> */}
   </React.StrictMode>
 );
